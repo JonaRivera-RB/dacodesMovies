2d9a1b68c77fd4cc76a676af654c407e8128f37d
Jonathan Rivera <jonathan@vetta.io>
2020-10-15 02:53:56 -0500

    Initial Commit

---
+++ DaCodesMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DaCodesMovies
+//
+//  Created by Jonathan Rivera Misael on 15/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DaCodesMoviesTests/DaCodesMoviesTests.swift
@@ -0,0 +1,33 @@
+//
+//  DaCodesMoviesTests.swift
+//  DaCodesMoviesTests
+//
+//  Created by Jonathan Rivera Misael on 15/10/20.
+//
+
+import XCTest
+@testable import DaCodesMovies
+
+class DaCodesMoviesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
